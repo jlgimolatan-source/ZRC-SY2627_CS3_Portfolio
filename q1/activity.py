f050c233ed8ca3c1858255fdcf227b3548842156
Jane Gimolatan <jlgimolatan@zrc.pshs.edu.ph>
2026-09-22 17:26:06 +0800

    smth

---
+++ q1/activity.py
@@ -0,0 +1,66 @@
+class AssignmentSubmission:
+    def __init__(self, student_name, student_id, assignment_title, due_date):
+        self.student_name = student_name
+        self.student_id = student_id
+        self._assignment_title = assignment_title
+        self._due_date = due_date
+        self.__is_submitted = False
+        self.__grade = 0
+        self.__submitted_files = []
+
+    def __validate_grade(self, score):
+        if grade > 0:
+            print("Graded.")
+        else:
+            print("Not Graded.")
+
+    def __check_submittion_status(self):
+        if len(self._AssignmentSubmission__submitted_files) == 0:
+            is_submitted = False
+        else:
+            status = len(self._AssignmentSubmission__submitted_files)
+            return(f"Submitted {status} files")
+        
+
+    def __is_duplicate(self, filename):
+        if len(submitted_files) != len(set(submitted_files)):
+            print("Your submitted files has duplicates, please remove the duplicate.")
+        else:
+            print("Your submission does not have duplicates, please proceed.")
+
+    def add_file(self, filename):
+        self.__submitted_files.append(filename)
+        
+
+    def remove_file(self, filename):
+        self.__submitted_files.remove(filename)
+
+    def assign_grade(self, score):
+        self.grade = score
+        
+    def get_grade(self):
+        print(grade)
+        
+    def view_files(self):
+        files = submitted_files
+        print(files)
+
+    def get_status_report(self):
+        status = self._AssignmentSubmission__check_submittion_status()
+        list = [self.student_id, self.student_name, status, self.grade]
+        return("ID:", list[0], "| Name:", list[1], "| Status:", list[2], "| Grade:", self.grade)
+
+print("--- INITIA"
+student1 = AssignmentSubmission("Alex Gonzaga", "pshs-1090-x", "CS-101", "2026-10-01")
+student2 = AssignmentSubmission("Adelle", "pshs-1920-x", "CS-103", "2026-10-01")
+student3 = AssignmentSubmission("Juan dela Cruz", "pshs-1968-x", "CS-104", "2026-10-01")
+student2 = AssignmentSubmission("Maria Santos", "pshs-1967-x", "CS-105", "2026-10-01")
+student2 = AssignmentSubmission("Jose Reyes", "pshs-1978-x", "CS-106", "2026-10-01")
+print()
+
+student1.add_file("main.py")
+student1.add_file("report.pdf")
+student1.assign_grade(95)
+
+
+student1.get_status_report()
